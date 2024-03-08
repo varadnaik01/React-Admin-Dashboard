ef9756eea3a698fa7da22e7ed86765d3db79eba3
Varad Naik <varadchandrashekha@gmail.com>
2024-03-08 21:24:49 +0530

    second commit

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -108,7 +108,7 @@ const Sidebar = () => {
                   fontWeight="bold"
                   sx={{ m: "10px 0 0 0" }}
                 >
-                  Ed Roh
+                  Varad Naik
                 </Typography>
                 <Typography variant="h5" color={colors.greenAccent[500]}>
                   VP Fancy Admin
